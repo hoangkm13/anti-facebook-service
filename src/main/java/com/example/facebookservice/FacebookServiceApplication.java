567ed56c81343b88503a809c290f4a858dc0cdf3
hoangkm13 <hoang.workingemail@gmail.com>
2023-10-10 13:33:08 +0700

    init

---
+++ src/main/java/com/example/facebookservice/FacebookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.facebookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/facebookservice/FacebookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.facebookservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
