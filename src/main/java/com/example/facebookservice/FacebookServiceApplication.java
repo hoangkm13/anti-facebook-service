7bc78701c0f91a6a6a13b6d2d4398a8360744205
hoangkm13 <hoang.workingemail@gmail.com>
2023-10-10 13:34:15 +0700

    refactor code

--- src/main/java/com/example/facebookservice/FacebookServiceApplication.java
+++ src/main/java/com/example/facebookservice/FacebookServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FacebookServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FacebookServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(FacebookServiceApplication.class, args);
+    }
 
 }
